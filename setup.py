922e368c7aa8a3c914e7a20a4cd0578a3d9b4c40
Jonny <jonathanomahony@gmail.com>
2020-10-21 10:41:50 +0100

    Fix long_description, bump to v0.1.1

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="Evervault",
-    version="0.1.0",
+    version="0.1.1",
     author="Jonny O'Mahony",
     author_email="jonny@evervault.com",
     url="https://evervault.com",
     license="LICENSE.txt",
     description="Evervault SDK",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    long_description=open("README.md").read(),
     packages=find_packages(),
     install_requires=["pytest", "requests", "cryptography", "certifi", "pycryptodome"],
 )
