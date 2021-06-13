14e59f0c1692bca5b7c3a6f59b606d9c6faac14a
David Nugent <david.nugent2425@gmail.com>
2021-06-13 13:25:31 +0100

    working but could be more elegant

--- evervault/__init__.py
+++ evervault/__init__.py
@@ -10,6 +10,7 @@ api_key = None
 request_timeout = 30
 base_url = "https://api.evervault.com/"
 base_run_url = "https://cage.run/"
+outbound_relay_url="https://relay.evervault.com:443"
 
 
 def run(cage_name, encrypted_data, options = { "async": False, "version": None }):
@@ -27,6 +28,9 @@ def encrypt_and_run(cage_name, data, options = { "async": False, "version": None
 def cages():
     return __client().cages()
 
+def outbound_relay():
+    __client().outbound_relay()
+
 
 def __client():
     if not api_key:
@@ -38,6 +42,7 @@ def __client():
             request_timeout=request_timeout,
             base_url=base_url,
             base_run_url=base_run_url,
+            outbound_relay_url=outbound_relay_url,
         )
         return ev_client
     else:
