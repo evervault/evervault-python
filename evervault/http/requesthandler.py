c4a6770c0a3a528a2ccd0fa670a360780e929aa7
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-03-28 22:00:31 -0700

    Small refactor

--- evervault/http/requesthandler.py
+++ evervault/http/requesthandler.py
@@ -6,43 +6,33 @@ class RequestHandler(object):
         self.request = request
 
     def get(self, path, params={}, check_cert=False):
-        if self.cert.is_certificate_expired():
-            self.cert.setup()
         if check_cert:
-            self.__assert_certificate_available()
-
+            self.__validate_certificate()
         return self.request.make_request("GET", self.__url(path), params)
 
     def post(self, path, params, optional_headers, cage_run=False, check_cert=False):
-        if self.cert.is_certificate_expired():
-            self.cert.setup()
         if check_cert:
-            self.__assert_certificate_available()
-
+            self.__validate_certificate()
         return self.request.make_request(
             "POST", self.__url(path, cage_run), params, optional_headers
         )
 
     def put(self, path, params, check_cert=False):
-        if self.cert.is_certificate_expired():
-            self.cert.setup()
         if check_cert:
-            self.__assert_certificate_available()
-
+            self.__validate_certificate()
         return self.request.make_request("PUT", self.__url(path), params)
 
     def delete(self, path, params, check_cert=False):
-        if self.cert.is_certificate_expired():
-            self.cert.setup()
         if check_cert:
-            self.__assert_certificate_available()
-
+            self.__validate_certificate()
         return self.request.make_request("DELETE", self.__url(path), params)
 
     def __url(self, path, cage_run=False):
         base_url = self.base_run_url if cage_run else self.base_url
         return base_url + path
 
-    def __assert_certificate_available(self):
+    def __validate_certificate(self):
         if self.request.http_session.cert is None:
             raise Exception("Certificate not available")
+        if self.cert.is_certificate_expired():
+            self.cert.setup()
