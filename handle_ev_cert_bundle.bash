91201478ff6746a3d3719ff0da11db2602ae3631
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-05-04 15:41:47 -0300

    trying to deal with it

--- handle_ev_cert_bundle.bash
+++ handle_ev_cert_bundle.bash
@@ -4,7 +4,15 @@ POETRY_ENV=$(poetry env info --path)
 VERSION=$(python -V 2>&1 | cut -d\  -f 2)
 VERSION=(${VERSION//./ })
 
-CERT_FILE="${POETRY_ENV}/lib/python${VERSION[0]}.${VERSION[1]}/site_packages/certifi/cacert.pem"
+CERT_FILE="${POETRY_ENV}/lib/python${VERSION[0]}.${VERSION[1]}/site-packages/certifi/cacert.pem"
+UBUNTU_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
 
-echo ${CERT_FILE}
+echo "----------------------- CERT_FILE"
+cat ${CERT_FILE}
+echo "----------------------- UBUNTU_FILE"
+cat ${UBUNTU_FILE}
+echo "-----------------------"
 
+# removing virtual env file and replacing it with OS bundle
+rm ${CERT_FILE}
+ln -s ${UBUNTU_BUNDLE} ${CERT_FILE}
