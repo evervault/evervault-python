47413a920debe8e0606799932a39fc2767c41853
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-05-03 23:03:22 -0300

    trying to use SSL_CERT_DIR

--- handle_ev_certificate.bash
+++ handle_ev_certificate.bash
@@ -3,8 +3,5 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https curl ca-certificates
 
-wget https://ca.evervault.io --output ~/evervault.io.pem
-
 sudo curl https://ca.evervault.io --output /usr/local/share/ca-certificates/evervault.io.crt
-find /usr/local/share/ca-certificates
 sudo update-ca-certificates
