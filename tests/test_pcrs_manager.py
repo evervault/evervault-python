b1ed24e3ece105c81d515ab89e06f79ba1ff3a7f
Donal Tuohy <donaltuohy@gmail.com>
2023-10-31 17:53:31 +0000

    [SPE-925] Add pcr provider manager for cage pcrs

--- tests/test_pcrs_manager.py
+++ tests/test_pcrs_manager.py
@@ -60,34 +60,3 @@ class TestCagePcrManager(unittest.TestCase):
         manager.remove_pcrs_for_cage(self.cage_1)
 
         assert manager.get(self.cage_1) == test_pcrs1
-
-    # @requests_mock.Mocker()
-    # def test_get_attestation_doc_missing_cache_reload(self, mock_request):
-    #     self.cage_host = "cage.evervault.com"
-    #     self.__mock_attestation_doc(mock_request, self.cage_1)
-    #     self.__mock_attestation_doc(mock_request, self.cage_2)
-    #     cage_names = [self.cage_1]
-    #     doc = AttestationDoc(self.app_uuid, cage_names, "cage.evervault.com")
-    #     assert doc.get(self.cage_1) == "123"
-    #     assert doc.get(self.cage_2) == "123"
-
-    # @responses.activate
-    # def test_get_attestation_doc_cache_poll(self):
-    #     responses.add(
-    #         responses.GET,
-    #         f"https://cage_1.{self.app_uuid}.cage.evervault.com/.well-known/attestation",
-    #         json={"attestation_doc": "123"},
-    #         status=200,
-    #     )
-    #     responses.add(
-    #         responses.GET,
-    #         f"https://cage_1.{self.app_uuid}.cage.evervault.com/.well-known/attestation",
-    #         json={"attestation_doc": "newdoc!"},
-    #         status=200,
-    #     )
-    #     self.cage_host = "cage.evervault.com"
-    #     cage_names = [self.cage_1]
-    #     doc = AttestationDoc(self.app_uuid, cage_names, "cage.evervault.com", 1)
-    #     assert doc.get(self.cage_1) == "123"
-    #     time.sleep(2)
-    #     assert doc.get(self.cage_1) == "newdoc!"
