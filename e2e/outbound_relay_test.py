edddbf8109019f888543311771100925a9b65490
Eoin Power-Moran <eoinpm@evervault.com>
2024-02-06 11:30:44 +0000

    query params for synthetic endpoint

--- e2e/outbound_relay_test.py
+++ e2e/outbound_relay_test.py
@@ -12,7 +12,8 @@ class OutboundRelayTest(EndToEndTestCase):
         }
         headers = {"Content-Type": "application/json"}
 
-        response = self.make_request(self.syntheticEndpointurl, headers, payload)
+        synthetic_endpoint = self.syntheticEndpointurl + "/syntheticUuid=python-e2e&mode=inbound"
+        response = self.make_request(synthetic_endpoint, headers, payload)
         print(response)
         self.assertEqual(response["request"]["string"], False)
         self.assertEqual(response["request"]["number"], False)
